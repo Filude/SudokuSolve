4e900f03f6d4267a179e9eaf2bd5742cf667447c
Ffion <finnvdlugt@gmail.com>
2022-12-22 22:52:54 +0100

    Additions for testing
    Added a function to return the sudokus parameters, and a bit to the main program to do a bunch of attempts at the same puzzle to get an estimate of the average iterations needed

--- Sudoku.cs
+++ Sudoku.cs
@@ -295,8 +295,12 @@
             }
 
         }
+        public string parameters()
+        {
+            return "\nS: " + this.S + "\nviewDistance: " + this.viewDistance + "\nrandomWalkDifferenceTrigger: " + this.randomWalkDifferenceTrigger;
+        }
 
-        public void solve() {
+        public int solve() {
 
             int counter = 0; // Iteration counter
             List<int> lastScores = new List<int>(); // List with last seen scores
@@ -342,6 +346,7 @@
             print();
 
             Console.WriteLine("Solved in " + counter.ToString() + " iterations.");
+            return counter;
 
         }
 
